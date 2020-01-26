5f2f547779ae59581973f89e120fbf7d4995b98c
guimevn <54328227+guimevn@users.noreply.github.com>
2020-01-26 13:43:10 -0300

    Cálculo de velocidade de download

--- 01_estruturaSequencial/ex018.py
+++ 01_estruturaSequencial/ex018.py
@@ -5,3 +5,4 @@ mb = float(input('Qual o tamanho do arquivo para download? (mB) '))
 mbps = float(input('Qual a velocidade de download? (mbps) '))
 velD = mb / (mbps / 8)
 print('Você levará {:.2f}min para baixar o arquivo.'.format(velD / 60))
+ 
