908b9dfebf9c35b2d29f9cb7097b376a8bef53a1
guimevn <54328227+guimevn@users.noreply.github.com>
2020-01-26 13:34:45 -0300

    Delete ex006.py

--- ex006.py
+++
@@ -1,6 +0,0 @@
-import math
-""""Faça um Programa que peça o raio de um círculo, calcule e mostre sua área."""
-
-r = int(input('Digite o raio do círculo: '))
-a = math.pi * r ** 2
-print('A área do círculo com o raio {} é igual a: {:.2f}'.format(r, a))
