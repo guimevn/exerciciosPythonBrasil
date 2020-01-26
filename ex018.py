38a46971fff676b367b0786c06826a5e4a52268b
guimevn <54328227+guimevn@users.noreply.github.com>
2020-01-26 13:36:08 -0300

    Delete ex018.py

--- ex018.py
+++
@@ -1,7 +0,0 @@
-"""Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um
-link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos)."""
-
-mb = float(input('Qual o tamanho do arquivo para download? (mB) '))
-mbps = float(input('Qual a velocidade de download? (mbps) '))
-velD = mb / (mbps / 8)
-print('Você levará {:.2f}min para baixar o arquivo.'.format(velD / 60))
