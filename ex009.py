415ac13a1529794024bedd090001b6d72c1a51db
guimevn <54328227+guimevn@users.noreply.github.com>
2020-01-26 13:35:03 -0300

    Delete ex009.py

--- ex009.py
+++
@@ -1,5 +0,0 @@
-""""Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius."""
-
-f = int(input('Digite a temperatura em graus Farenheit: '))
-c = (f - 32) * 5/9
-print('A temperatura em graus Celsius é: {:.2f}'.format(c))
